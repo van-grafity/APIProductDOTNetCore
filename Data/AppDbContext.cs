7d5f1ba6c349d805b573148d049bde1f13b4541b
Ivan Suhendra <38711246+van-grafity@users.noreply.github.com>
2024-07-23 16:24:16 +0700

    added model and database context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using APIProductDOTNetCore.Models;
+
+namespace APIProductDOTNetCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
